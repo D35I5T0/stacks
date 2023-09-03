fa9a98350d3d2d1099a16c6c301d6682202dff4e
Celso Netto <132786359+D35I5T0@users.noreply.github.com>
2023-09-03 03:28:30 -0300

    Update App.java

--- App.java
+++ App.java
@@ -3,47 +3,21 @@ import java.util.Scanner;
 public class App {
     public static void main (String[] args) {
         Scanner sc = new Scanner (System.in);
-        int op, valor;
+        System.out.println("Digite uma frase: ");
+        String frase = sc.nextLine();
+        int tam = frase.length();
+        stackChar pilha = new stackChar(tam);
 
-        System.out.println("Informe o tamanho do vetor");
-        int tam = sc.nextInt();
-        stackInt pilha = new stackInt(tam);
-
-        do {
-            menu();
-            op = sc.nextInt();
-            switch (op) {
-                case 1:
-                    System.out.println("Informe o número: ");
-                    valor = sc.nextInt();
-                    pilha.push(valor);
-                    break;
-                case 2:
-                    System.out.println(valor = pilha.pop()); 
-                    break;
-                case 3:
-                    System.out.println(valor = pilha.top()); 
-                    break;
-                case 4:
-                    pilha.exibirLista();
-                    break;
-                case 0:
-                    System.out.println("Fim de programa");
-                    break;
-                default:
-                    System.out.println("Tente de novo");
-                    break;
+        for (int i = 0; i < tam; i++) {
+            char letra = frase.charAt(i);
+            if (Character.isLetter(letra)) {
+                pilha.push(letra);
             }
-
-        } while (op != 0);
-
+        }
+        while (!pilha.isEmpty()) {
+            System.out.print(pilha.pop());
+        }
         sc.close();
     }
-    public static void menu() {
-        System.out.println("1 - Colocar numero na pilha");
-        System.out.println("2 - Retirar numero da pilha");
-        System.out.println("3 - Mostrar numero do topo da pilha");
-        System.out.println("4 - Exibir pilha");
-        System.out.println("0 - Finalizar programa");
-    }
 }
+
